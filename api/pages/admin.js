874db64f9e5f7eb29f11983ecf84c12da3259caf
OğuzAras <oguzhamurculu@me.com>
2026-04-20 15:54:12 +0300

    Create admin.js

---
+++ api/pages/admin.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'admin.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(html);
+}
